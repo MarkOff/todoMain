0c015603c2a6a707b5861bcda5ae178d9904ef1d
MarkoVVV <sam.samlin@mail.ru>
2023-05-01 21:02:58 +0300

    migrated on RTK

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,72 +1,42 @@
-import React, {useEffect} from 'react'
-import './App.css'
-import {TodolistsList} from '../features/TodolistsList/TodolistsList'
-import {useAppDispatch, useAppSelector} from './store'
-import {RequestStatusType} from './app-reducer'
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import Container from '@mui/material/Container';
-import LinearProgress from '@mui/material/LinearProgress';
-import {Menu} from '@mui/icons-material';
-import {ErrorSnackbar} from '../components/ErrorSnackbar/ErrorSnackbar'
-import {Login} from "../features/login/Login";
-import {Navigate, Route, Routes} from "react-router-dom";
-import {logOutTC, meTC} from "../features/login/auth-reducer";
-import {CircularProgress} from "@mui/material";
-
+import React, {useEffect} from 'react';
+import {useSelector} from 'react-redux';
+import {BrowserRouter} from 'react-router-dom';
+import {CircularProgress,} from '@mui/material';
+import './App.css';
+import {ErrorSnackbar} from 'common/components';
+import {useActions} from 'common/hooks';
+import {selectIsInitialized} from 'app/app.selectors';
+import {authThunks} from 'features/auth/auth.reducer';
+import {Header} from 'app/Header/Header';
+import {Routing} from 'app/Routing/Routing';
 
 function App() {
-    const dispatch = useAppDispatch()
-    const status = useAppSelector<RequestStatusType>((state) => state.app.status)
-    const isInitialized = useAppSelector<boolean>((state) => state.auth.isInitialized)
-    const isLoggedIn = useAppSelector(state => state.auth.isLoggedIn)
 
-    const logOutHandler = () => {
-        dispatch(logOutTC())
-    }
+    const isInitialized = useSelector(selectIsInitialized);
+    const {initializeApp} = useActions(authThunks);
 
     useEffect(() => {
-        dispatch(meTC())
-    }, [])
+        initializeApp({});
+    }, []);
 
 
     if (!isInitialized) {
-        return <div
-            style={{position: 'fixed', top: '30%', textAlign: 'center', width: '100%'}}>
-            <CircularProgress/>
-        </div>
+        return (
+            <div style={{position: 'fixed', top: '30%', textAlign: 'center', width: '100%'}}>
+                <CircularProgress/>
+            </div>
+        );
     }
 
-
-
     return (
-        <div className="App">
-            <ErrorSnackbar/>
-            <AppBar position="static">
-                <Toolbar>
-                    <IconButton edge="start" color="inherit" aria-label="menu">
-                        <Menu/>
-                    </IconButton>
-                    <Typography variant="h6">
-                        News
-                    </Typography>
-                    { isLoggedIn && <Button color="inherit" onClick={logOutHandler}>Log out</Button>}
-                </Toolbar>
-                {status === 'loading' && <LinearProgress/>}
-            </AppBar>
-            <Container fixed>
-                <Routes>
-                    <Route path={'/'} element={<TodolistsList/>}/>
-                    <Route path={'/login'} element={<Login/>}/>
-                    <Route path={'/404'} element={<h1 style={{textAlign: 'center'}}>PAGE NOT FOUND 404</h1>}/>
-                    <Route path={'*'} element={<Navigate to={'404'}/>}/>
-                </Routes>
-            </Container>
-        </div>
-    )
+        <BrowserRouter>
+            <div className="App">
+                <ErrorSnackbar/>
+                <Header/>
+                <Routing/>
+            </div>
+        </BrowserRouter>
+    );
 }
 
-export default App
+export default App;
